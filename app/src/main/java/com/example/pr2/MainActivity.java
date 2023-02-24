8c95765d4085d0d336e1ead84b86aa0a8e8ea649
Kate <ggkate12321@gmail.com>
2023-02-24 20:41:12 +0300

    third

--- app/src/main/java/com/example/pr2/MainActivity.java
+++ app/src/main/java/com/example/pr2/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.pr2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,9 +23,14 @@ public class MainActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(getApplicationContext(), "Текст", Toast.LENGTH_SHORT);
         toast.show();
+
     }
+
+
     @Override
     protected void onResume(){
+
+
         setContentView(R.layout.activity_main);
 
         Toast toast = Toast.makeText(getApplicationContext(), "Текст", Toast.LENGTH_SHORT);
@@ -27,6 +41,26 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "подробности");
         Log.e(TAG, "ошибка");
         Log.w(TAG, "предупреждение");
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("kdm");
+                Log.i(TAG, "Вы нажали на кнопку, программно");
+            }
+        });
+
     }
+
+    public void Messages(View view){
+        Log.i(TAG, "Вы нажали на кнопку, декларативно");
+    }
+
+
+
 }
+
 
